b5e954eb075db64533b615e4d5c403bea8a83fa4
Marco Tristao <marcotuliopin@gmail.com>
2022-07-13 21:20:56 -0300

    define seg tree as a namespace

--- data-structures/seg-tree.cpp
+++ data-structures/seg-tree.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//usando lazy propagation
-template<typename T>
-struct segtree {
-    vector<T> tree;
-    vector<T> lazy;
+namespace segtree {
+    vector<int> tree;
+    vector<int> lazy;
     int n;
 
-    segtree(int n_ = 1) : n{n_} {
-        tree = vector<T>(2*n);
-        lazy = vector<T>(2*n, 0);
-    }
-    void build (vector<T> v) {
+    void build (vector<int> v) {
         for (int i = n; i < 2*n; i++)
             tree[i] = v[i - n];
         for (int i = n - 1; i >= 0; i--)
             tree[i] = tree[2*i] + tree[2*i + 1];
+    }
+    void propagate(int pos, int begin, int end) {
+        int mid = (begin + end)/2;
+        lazy[2*pos] += lazy[pos];
+        tree[2*pos] += (mid - begin + 1)*lazy[pos];
+        lazy[2*pos + 1] += lazy[pos];
+        tree[2*pos + 1] += (end - mid)*lazy[pos];
+        lazy[pos] = 0;
     }
     void update(int l, int r, int x, int begin, int end, int pos = 0) {
         if (end < l || r < begin)
@@ -32,14 +34,6 @@ struct segtree {
         update(l, r, x, begin, mid, 2*pos);
         update(l, r, x, mid + 1, end, 2*pos + 1);
         tree[pos] = tree[2*pos] + tree[2*pos + 1];
-    }
-    void propagate(int pos, int begin, int end) {
-        int mid = (begin + end)/2;
-        lazy[2*pos] += lazy[pos];
-        tree[2*pos] += (mid - begin + 1)*lazy[pos];
-        lazy[2*pos + 1] += lazy[pos];
-        tree[2*pos + 1] == (end - mid)*lazy[pos];
-        lazy[pos] = 0;
     }
     int search(int l, int r, int begin, int end, int pos = 0) {
         if (l <= begin && end <= r)
